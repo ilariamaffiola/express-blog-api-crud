ebcb00327dbf5ee4f9c3027ed9851f17bb7b935e
Ilaria <maffiolailaria1@gmail.com>
2025-04-22 16:35:11 +0200

    [FEAT]enhance show function to return dish details and handle not found errors

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -5,8 +5,19 @@ function index(req,res){
     res.json(menu);
 }
 function show (req,res){
-    res.send(`dettaglio della pietanza ${req.params.id}`); 
-}
+    //res.send(`dettaglio della pietanza ${req.params.id}`);
+    const id = parseInt(req.params.id);
+    const pietanza = menu.find(pietanza => pietanza.id === id);
+    res.json(pietanza);
+    console.log(pietanza);
+    if(pietanza===undefined){
+        res.status(404);
+        res.json({
+            "error":"not found",
+            "message": "la pietanza non è presente nel menù",
+        });
+    };
+};
 function store (req,res){
     res.send('crea una nuova pietanza');
 }
