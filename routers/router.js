1061c543509fe1c10fd7510a786f44cc63f54119
Ilaria <maffiolailaria1@gmail.com>
2025-04-22 16:07:47 +0200

    [FEAT]add CRUD operations for managing dishes in router

--- routers/router.js
+++ routers/router.js
@@ -10,10 +10,22 @@ router.get('/', (req,res) => {
 router.get('/:id', (req,res) => {
     res.send(`dettaglio della pietanza ${req.params.id}`); 
 });
-
-
-
-
+//store
+router.post('/', (req,res) => {
+    res.send('crea una nuova pietanza');
+});
+//update
+router.put('/:id', (req,res) =>{
+    res.send(`modfica totale della pietanza ${req.params.id}`);
+});
+//modify
+router.patch('/:id', (req,res) => {
+    res.send(`modfica parziale della pietanza ${req.params.id}`);
+});
+//destroy
+router.delete('/:id', (req,res) => {
+    res.send(`elimina la pietanza ${req.params.id}`);
+});
 
 
 
