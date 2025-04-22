43eb9bbe32e5990b075bc6d813a9d79eb0943cf1
Ilaria <maffiolailaria1@gmail.com>
2025-04-22 15:59:55 +0200

    [FEAT]implement index and show routes in router

--- routers/router.js
+++ routers/router.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
+//index
+router.get('/', (req,res) => {
+    res.send('Lista delle pietanze');
+});
+//show
+router.get('/:id', (req,res) => {
+    res.send(`dettaglio della pietanza ${req.params.id}`); 
+});
 
 
 
